93be6bb8ad61cb8d44d0e617b83ccf3a204d79db
NahianSalsabil <46007553+NahianSalsabil@users.noreply.github.com>
2020-09-15 23:43:45 +0600

    Update Artist.sql

--- TermProject/TermProject/Database/Artist.sql
+++ TermProject/TermProject/Database/Artist.sql
@@ -1,6 +1,6 @@
 DROP TABLE "Artist";
 CREATE TABLE "Artist" (
-  "aID " NUMBER NOT NULL,
+  "aID" NUMBER NOT NULL,
   "Name" VARCHAR2(30) NOT NULL,
   "Gender" VARCHAR2(20) NOT NULL,
   "Birth_Date" DATE NOT NULL,
@@ -8,6 +8,6 @@ CREATE TABLE "Artist" (
   "Birth_Place" VARCHAR2(30) NOT NULL,
   "Death_Date" DATE,
   "Photo" BLOB NOT NULL,
-  CONSTRAINT "ArtistPK" PRIMARY KEY ("aID ")
+  CONSTRAINT "ArtistPK" PRIMARY KEY ("aID")
 );
 
