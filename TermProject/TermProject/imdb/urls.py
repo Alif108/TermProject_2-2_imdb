eb8c9d2a71ae6097909425d5c3faf6c9a7dd841f
Alif108 <46017890+Alif108@users.noreply.github.com>
2020-10-01 13:29:41 +0600

    Admin input updated

--- TermProject/TermProject/imdb/urls.py
+++ TermProject/TermProject/imdb/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('movie/', views.movie, name='movie'),
     path('login/', views.login, name="login"),
     path('create_account/', views.create, name="create_account"),
+    path('admin_update/', views.admin_update, name="admin_update"),
+    path('admin_movie_form/', views.admin_movie_form, name="admin_movie_form"),
+    path('admin_show_form/', views.admin_show_form, name="admin_show_form"),
+    path('admin_genre_form/', views.admin_genre_form, name="admin_genre_form"),
+    path('admin_artist_form/', views.admin_artist_form, name="admin_artist_form"),
+    path('admin_director_form/', views.admin_director_form, name="admin_director_form"),
 ]
 
 
